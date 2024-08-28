b23950eb1fc5e9c3ebe5d8ec7f97aeef99994da3
teerasak <156870591+Teerasak-dv@users.noreply.github.com>
2024-08-28 22:54:33 +0700

    Create GC2.ino

---
+++ GC2/GC2.ino
@@ -0,0 +1,27 @@
+float vp = 1.7;
+float r1 = 100000;
+float r2 = 10000;
+float ts = 0;
+
+void setup() {
+
+Serial.begin(9600);
+Serial.println("CLEARDATA");
+Serial.println("LABEL");
+Serial.println("RESETTIMER");
+
+Serial.println((int) (vp / (r2 / (r1 + r2))));
+
+delay(1000);
+}
+void loop() {
+float v = (analogRead (0) * vp) / 1024.0;
+float v2 = ((v / (r2 / (r1 + r2))*10)+1); // here you can calibrate the sensor
+if (v2 <2) {
+v2 = 0.00;
+
+}
+Serial.print("DATA,TIME,TIMER,");
+Serial.println(v2);
+delay (1000);
+}
