70057cd1479982345aa92ea6826c02fead3cf919
Teerasak Tonseenon <156870591+Teerasak-dv@users.noreply.github.com>
2024-09-04 10:51:49 +0700

    Update GC04.ino

--- Arduino Code/GC04/GC04.ino
+++ Arduino Code/GC04/GC04.ino
@@ -27,10 +27,10 @@ void setup() {
 
 void bt() {
   bt.println("RESETTIMER");
-  ststate = 1;
   bt.println("start");
-  digitalWrite(ledst, ststate);
   while(digitalRead(resetBT) != 0) {
+    statate = !ststate;
+    digitalWrite(ledst, ststate);
     float v = (analogRead(0) * 5) / 1024.0;
     bt.print("DATA,TIME,TIMER,");
     bt.println(v);
@@ -38,12 +38,12 @@ void bt() {
       sum = sum + v;
     }
     delay(1000);
+    statate = !ststate;
+    digitalWrite(ledst, ststate);
   }
-  ststate = 0;
   bt.print("reset Sum = ");
   bt.println(sum);
   sum = 0.0;
-  digitalWrite(ledst, ststate);
   delay(200);
 }
 
